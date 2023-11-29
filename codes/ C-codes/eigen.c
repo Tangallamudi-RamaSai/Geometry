226220816f259433d14320803ca5c7e5604a547b
Tangallamudi-RamaSai <105980937+Tangallamudi-RamaSai@users.noreply.github.com>
2023-11-29 14:31:16 +0530

    Update eigen.c

--- codes/ C-codes/eigen.c
+++ codes/ C-codes/eigen.c
@@ -28,61 +28,16 @@ R_o=loadMat("R.dat",2,2);
 C_alt=loadMat("C_alt.dat",3,3);
 C_in=loadMat("C_in.dat",3,3);
 
-
-
-	// **********************VECTORS*****************************
-	double **G_dir=Matmul(G_v,C_m,2,3,3);
-	double **G_n=Matmul(R_o,G_dir,2,2,3);
-	double **G_con=diag(Matmul(transposeMat(G_n,2,3),G_v,3,2,3),3,3);
-	double **G_dis=sqrt_diag(Matmul(transposeMat(G_dir,2,3),G_dir,3,2,3),3,3);
-	double **G_line=Mathstack(transposeMat(G_n,2,3),transposeMat(G_con,1,3),3,2,1);
-	//***********************MEDIANS*******************************
-	double **G_mid=Matscale(Matmul(G_v,C_mid,2,3,3),2,3,0.5);
-	double **G_med_dir=Matmul(G_v,C_mid_dir,2,3,3);
-	double **G_n_med = Matmul(R_o,G_med_dir,2,2,3);
-	double **cmat_med=diag(Matmul(transposeMat(G_n_med,2,3),G_v,3,2,3),3,3);
-	double **linemat_med=Mathstack(transposeMat(G_n_med,2,3),transposeMat(cmat_med,1,3),3,2,1);	
-	double **G_G=line_intersect(linemat_med,3,3);
-	//***********************ALTITUDE*******************************
-	double **G_n_alt=Matmul(G_v,C_alt,2,3,3);
-	double **cmat_alt=diag(Matmul(transposeMat(G_n_alt,2,3),G_v,3,2,3),3,3);
-	double **linemat_alt=Mathstack(transposeMat(G_n_alt,2,3),transposeMat(cmat_alt,1,3),3,2,1);	
-	double **G_H=line_intersect(linemat_alt,3,3);
-	//******************PERPENDICULAR BISECTOR**********************
-	double **cmat_perp_bis=diag(Matmul(transposeMat(G_n_alt,2,3),G_mid,3,2,3),3,3);
-	double **linemat_perp_bis=Mathstack(transposeMat(G_n_alt,2,3),transposeMat(cmat_perp_bis,1,3),3,2,1);	 
-	double **G_O=line_intersect(linemat_perp_bis,3,3);
-	//********************ANGULAR  BISECTOR************************
-        double **secvec=Matscale(Matmul(G_dis,C_in, 1,3,3),1,3,0.5);
-        i_con[0][0]=  -1 ;      i_con[0][1]=secvec[0][2]/G_dis[0][2];     i_con[0][2]= secvec[0][1]/G_dis[0][0] ;
-        i_con[1][0]=secvec[0][2]/G_dis[0][1] ;       i_con[1][1]= -1 ;       i_con[1][2]=secvec[0][0]/G_dis[0][0];
-        i_con[2][0]= secvec[0][1]/G_dis[0][1];      i_con[2][1]= secvec[0][0]/G_dis[0][2] ;    i_con[2][2]= -1 ;
-        double **a_dir=Matmul(G_v,i_con,2,3,3);
-        double **a_nor=Matmul(R_o,a_dir,2,2,3);
-        double **a_cof=diag(Matmul(transposeMat(a_nor,2,3),G_v,3,2,3),3,3);
-        double **a_line=Mathstack(transposeMat(a_nor,2,3),transposeMat(a_cof,1,3),3,2,1);
-        double **a_i = line_intersect(a_line,3,3);
-
-
-        cont_mat[0][0]=  0 ;    cont_mat[0][1]=secvec[0][2]/G_dis[0][2];     cont_mat[0][2]= secvec[0][1]/G_dis[0][0] ;
-        cont_mat[1][0]=secvec[0][2]/G_dis[0][1] ;       cont_mat[1][1]= 0 ;      cont_mat[1][2]=secvec[0][0]/G_dis[0][0];
-        cont_mat[2][0]= secvec[0][1]/G_dis[0][1];      cont_mat[2][1]= secvec[0][0]/G_dis[0][2] ;    cont_mat[2][2]= 0 ;
-        double **G_i=Matmul(G_v,cont_mat,2,3,3);
-        double **G_imid=Matscale(Matmul(G_i,C_mid,2,3,3),2,3,0.5);
-        double **G_idir_alt=Matmul(G_i,C_alt,2,3,3);
-        double **cmat_iperp_bis=diag(Matmul(transposeMat(G_idir_alt,2,3),G_imid,3,2,3),3,3);
-	double **linemat_imed=Mathstack(transposeMat(G_idir_alt,2,3),transposeMat(cmat_iperp_bis,1,3),3,2,1);
-        double **G_I=line_intersect(linemat_imed,3,3);
-	//*******************Eigen vector approach to find the contact points******************************
-         double **h=createMat(2,1);  
-        h[0][0]=G_v[0][0]-G_I[0][0];    h[1][0]=G_v[1][0]-G_I[0][1];
-        double **V=createMat(2,2);
-        V[0][0]=1;      V[0][1]=0;      V[1][0]=0;       V[1][1]=1;
-        double **u=createMat(2,1);
-        u[0][0]=G_I[0][0]-G_I[0][0];    u[1][0]=G_I[0][1]-G_I[0][1];
-        double **f=createMat(1,1);
-        f[0][0]=sqrt(pow(G_I[0][0]-G_i[0][0],2)+pow(G_I[0][1]-G_i[1][0],2));
-        f[0][0]=-f[0][0]*f[0][0];
+	
+double **h=createMat(2,1);  
+h[0][0]=G_v[0][0]-G_I[0][0];    h[1][0]=G_v[1][0]-G_I[0][1];
+double **V=createMat(2,2);
+V[0][0]=1;      V[0][1]=0;      V[1][0]=0;       V[1][1]=1;
+double **u=createMat(2,1);
+u[0][0]=G_I[0][0]-G_I[0][0];    u[1][0]=G_I[0][1]-G_I[0][1];
+double **f=createMat(1,1);
+f[0][0]=sqrt(pow(G_I[0][0]-G_i[0][0],2)+pow(G_I[0][1]-G_i[1][0],2));
+f[0][0]=-f[0][0]*f[0][0];
 double **gh=Matadd(Matadd(Matmul(Matmul(transposeMat(h,2,1),V,1,2,2),h,1,2,1), Matscale( Matmul(transposeMat(u,2,1),h,1,2,1),1,1,2) ,1,1),f,1,1);
 double **sigmat=Matsub(Matmul(Matadd(Matmul(V,h,2,2,1),u,2,1),transposeMat(Matadd(Matmul(V,h,2,2,1),u,2,1),2,1),2,1,2) ,Matscale(V,2,2,gh[0][0]) ,2,2);
 
@@ -123,63 +78,8 @@ double **F=Matadd(h,t2,2,1);
 printf("\n Vectors \n");
 printf("vertices matrix= \n");
 printMat(G_v,2,3);
-//printf("direction matrix= \n");
-//printMat(G_dir,2,3);
-//printf("normal matrix= \n");
-//printMat(G_n,2,3);
-//printf("constant matrix= \n");
-//printMat(G_con,1,3);
-//printf("distance matrix= \n");
-//printMat(G_dis,1,3);
-//printf("line  matrix= \n");
-//printMat(G_line,3,3);
-//
-//printf("\n Medians \n");
-//printf("midpoint matrix= \n");
-//printMat(G_mid,2,3);
-//printf("median direction  matrix= \n");
-//printMat(G_med_dir,2,3);
-//printf("median normal matrix= \n");
-//printMat(G_n_med,2,3);
-//printf("median constant matrix= \n");
-//printMat(cmat_med,1,3);
-//printf("median line matrix= \n");
-//printMat(linemat_med,3,3);
-//printf("Centroid = \n");
-//printMat(G_G,1,2);
-//
-//printf("\n Altitude \n");
-//printf("altitude normal matrix= \n");
-//printMat(G_n_alt,2,3);
-//printf("altitude constant matrix= \n");
-//printMat(cmat_alt,1,3);
-//printf("Altitude line matrix= \n");
-//printMat(linemat_alt,3,3);
-//printf("Orthocentre = \n");
-//printMat(G_H,1,2);
-//
-//printf("\n Perpendicular Bisector \n");
-//printf("perp_bisect constant  matrix= \n");
-//printMat(cmat_perp_bis,1,3);
-//printf("perp_bis line matrix= \n");
-//printMat(linemat_perp_bis,3,3);
-//printf("Circumcentre = \n");
-//printMat(G_O,1,2);
-//
-//printf("\n Angular Bisector \n");
-//printf("m,n,p values = \n");
-//printMat(secvec,1,3);
-//printf("ang_bis direction matrix= \n");
-//printMat(a_dir,2,3);
-//printf("ang_bis normal matrix= \n");
-//printMat(a_nor,2,3);
-//printf("ang_bis constant matrix= \n");
-//printMat(a_cof,1,3);
-//printf("ang_bis line  matrix= \n");
-//printMat(a_line,3,3);
-//printf("ang_bis intersection points= \n");
-//printMat(a_i,1,2);
-printf("Eigen vector Approach\n");
+
+printf("Eigen vector \n");
 printf("Incentre = \n");
 printMat(G_I,1,2);
 printf("contact points = \n");
