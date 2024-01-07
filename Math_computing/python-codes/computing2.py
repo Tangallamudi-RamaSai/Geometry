dd53443579171ce638511049fba52f09f3cac50e
Tangallamudi-RamaSai <105980937+Tangallamudi-RamaSai@users.noreply.github.com>
2024-01-07 09:08:08 +0530

    Add files via upload

--- Math_computing/python-codes/computing2.py
+++ Math_computing/python-codes/computing2.py
@@ -1,17 +1,13 @@
 import sys
-sys.path.insert(0,'/home/ramsai/MathComputing/codes/CoordGeo') #for path to external scripts
+sys.path.insert(0,'/home/ramsai/MathComputing/codes/CoordGeo') #for path to ext
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Function to read matrix from file
-def read_file(filename):
-    with open(filename, 'r') as file:
-        values = file.readline().strip().split(',')
-    return np.array([float(x) for x in values])
+# Reading matrix a from .dat file
+a = np.loadtxt('a.dat').reshape((2, 1))
 
-# Reading matrices a and b from .dat files
-a = read_file('a.dat').reshape((2, 1))
-b = read_file('b.dat').reshape((2, 1))
+# Reading matrix b from .dat file
+b = np.loadtxt('b.dat').reshape((2, 1))
 
 # Specifying the value of k
 k = 2
@@ -32,9 +28,10 @@ plt.plot([P[0, 0], Q[0, 0]], [P[1, 0], Q[1, 0]], label='PQ', linestyle='-')
 # Connecting Q to R with a dotted line
 plt.plot([Q[0, 0], R[0, 0]], [Q[1, 0], R[1, 0]], label='QR', linestyle='--')
 
-# Marking points P, Q, R on the lines with labels
-for point, label in zip([P, Q, R], ['P', 'Q', 'R']):
-    plt.text(*point, label, ha='right' if label == 'P' else 'left', va='bottom' if label == 'P' else 'top')
+# Marking points P, Q, R on the lines with labels without using a for loop
+plt.text(*P.flatten(), 'P', ha='right', va='bottom')
+plt.text(*Q.flatten(), 'Q', ha='right', va='top')
+plt.text(*R.flatten(), 'R', ha='left', va='top')
 
 # Print values for P, Q, R
 print("P:", P.flatten())
